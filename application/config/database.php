37b7197b274a0c638b51e21d5c3843552a2481d0
rezenderegis <rezenderegis@gmail.com>
2021-04-25 22:38:36 -0300

    Make some adjustments to deploy in production

--- application/config/database.php
+++ application/config/database.php
@@ -73,6 +73,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+//Desenvolvimento
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost:8889',
@@ -94,3 +96,28 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+//Produção
+/*
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'onecart.mysql.dbaas.com.br',
+	'username' => 'onecart',
+	'password' => 'Rnk30ghhh',
+	'database' => 'onecart',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+*/
