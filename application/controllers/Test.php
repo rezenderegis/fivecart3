3c0f8e70d2e5d3fde54ac5782d222a0ddde8d1b0
rezendereigis <rezenderegis@me.com>
2022-11-12 21:22:57 -0300

    Adjust controller test

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -9,10 +9,10 @@ class Test extends CI_Controller {
         parent::__construct();
         $this->load->helper('url');
 
-       /* if (!$this->ion_auth->logged_in()) {
+       if (!$this->ion_auth->logged_in()) {
             $this->session->set_flashdata('info', 'Sua sessão expirou');
             redirect ('login');
-        }*/
+        }
 
     }
 
