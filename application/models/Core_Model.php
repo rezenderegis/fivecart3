65339e43beda8eb8cc9ab8e721153b27e17a7f02
Thiago Luís <thiagoluismo@gmail.com>
2022-11-23 16:25:09 -0300

    Add consultas de produtos para cadastro e cadastrados.

--- application/models/Core_Model.php
+++ application/models/Core_Model.php
@@ -149,6 +149,29 @@ where pp.status =  1 and pp.id = ".$idProduct;
     return $query->result_array ();
 
     }
+    
+    public function getUserProductList($idPublish,$idUser,$term) {
+        $sql = "select p.name as text,pc.id,pc.price,p.image_link  from product_customer pc inner join products p on p.id = pc.id_product
+        where pc.id_user = ".$idUser."
+        and pc.id not in (select pc.id from product_publish pp inner join publish p on p.id = pp.id_publish
+        inner join product_customer pc on pc.id = pp.id_product_customer
+        inner join products prod on prod.id = pc.id_product
+        where pp.status = 1 and p.id = ".$idPublish.")". "and p.status = 1 and p.name like '$term%' order by p.name
+        limit 20";
+        $query = $this->db->query ( $sql );	
+        return $query->result_array();
+    }
+
+    /*public function getProductPublish($idPublish,$idUser) {
+        $sql = "select pc.id,p.description,pc.id_product,prod.name,pp.product_price price,prod.image_link, pp.id_publish, pp.id as id_product_publish,
+        p.id_user as id_user_publish, p.header2, prod.image_width, prod.image_height
+        from product_publish pp inner join publish p on p.id = pp.id_publish
+        inner join product_customer pc on pc.id = pp.id_product_customer
+        inner join products prod on prod.id = pc.id_product
+        where p.id_user = ".$idUser." and pp.status =  1 and p.id = ".$idPublish. " order by pp.id desc"; 
+        $query = $this->db->query ( $sql );	
+        return $query->result_array ();
+    }*/
 
 
     public function getUserProductsByName($idPublish,$idUser,$name) {
